e6b9eddcb8564ce272cddfd648e77b305cd1f35c
tatianno <tferreiraalves@gmail.com>
2025-01-20 18:05:21 -0300

    Adicionada documentacao swagger - openapi

---
+++ README.md
@@ -0,0 +1,7 @@
+cd /usr/src/
+
+git clone https://github.com/tatianno/ura-api.git
+
+cd ura-api
+
+bash install.sh
--- install.sh
+++ install.sh
@@ -2,12 +2,16 @@
 apt update && apt upgrade -y
 apt install python3 python3-pip python3-venv git nginx -y
 
-source venv/bin/activate
+sudo chown -R www-data:www-data /usr/src/ura-api/src
+
 python3 -m venv venv
+source venv/bin/activate
+
 pip install -r requirements.txt
-python manage.py migrate
-python manage.py collectstatic --noinput
-python manage.py createsuperuser
+
+python src/manage.py migrate
+python src/manage.py collectstatic --noinput
+python src/manage.py createsuperuser
 
 deactivate
 
@@ -16,5 +20,5 @@ systemctl daemon-reload
 systemctl start gunicorn
 systemctl enable gunicorn
 
-cp confs/nginx/default.conf /etc/nginx/conf.d/
+cp confs/nginx/default /etc/nginx/sites-available/
 /etc/init.d/nginx restart
