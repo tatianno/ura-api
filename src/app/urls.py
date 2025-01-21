10face7c325c6943f367e10884903098ad537c1f
tatianno <tferreiraalves@gmail.com>
2025-01-21 08:43:37 -0300

    Alterada view para rota padrao

--- src/app/urls.py
+++ src/app/urls.py
@@ -29,7 +29,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('atendimento/', include('atendimento.urls')),
     path('cliente/', include('cliente.urls')),
-    path('admin/', admin.site.urls, name='admin'),
     path('accounts/logout/',LogoutCustomView.as_view(), name='logout'),
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', admin.site.urls, name='admin'),
 ]
