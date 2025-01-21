bbd30e3731677bb78b2699af0c1a8d50ca9015d0
tatianno <tferreiraalves@gmail.com>
2025-01-21 08:54:35 -0300

    Adicionada url admin/

--- src/app/urls.py
+++ src/app/urls.py
@@ -29,6 +29,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('atendimento/', include('atendimento.urls')),
     path('cliente/', include('cliente.urls')),
+    path('admin/', admin.site.urls, name='admin'),
     path('accounts/logout/',LogoutCustomView.as_view(), name='logout'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('', admin.site.urls, name='admin'),
